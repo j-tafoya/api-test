28da75bbfd1e855a65ae76706ae335bc09a679c2
Johnathan Tafoya <65174118+j-tafoya@users.noreply.github.com>
2020-10-27 13:08:08 -0400

    Update helloworld.js

--- helloworld.js
+++ helloworld.js
@@ -3,3 +3,12 @@ var myArray = ['a', 'b', 'c', 'd'];
 
 delete myArray[2];  // Noncompliant. myArray => ['a', 'b', undefined, 'd']
 console.log(myArray[2]); // expected value was 'd' but output is undefined
+
+
+var myArray = ['a', 'b', 'c', 'd'];
+
+// removes 1 element from index 2
+removed = myArray.splice(2, 1);  // myArray => ['a', 'b', 'd']
+console.log(myArray[2]); // outputs 'd'
+
+if (1 == '1') { console.log("noncompliant")} // Noncompliant
