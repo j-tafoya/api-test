15fd7cfbe56bb3c24b6b233aa6f4c084bd7b7814
Johnathan Tafoya <65174118+j-tafoya@users.noreply.github.com>
2020-10-27 13:06:54 -0400

    Update helloworld.js

--- helloworld.js
+++ helloworld.js
@@ -1,1 +1,5 @@
 console.log("hello World!");
+var myArray = ['a', 'b', 'c', 'd'];
+
+delete myArray[2];  // Noncompliant. myArray => ['a', 'b', undefined, 'd']
+console.log(myArray[2]); // expected value was 'd' but output is undefined
