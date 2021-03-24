48ea176cea03f9a288dccd034ba50ddb4bb4fe34
Johnathan Tafoya <65174118+j-tafoya@users.noreply.github.com>
2021-03-24 11:31:47 -0400

    test

--- scripts/validate-licenses.sh
+++ scripts/validate-licenses.sh
@@ -36,10 +36,7 @@ lines=$(cat unapproved.csv | wc -l)
 if [ $lines > 1 ]
 then
     unapproved=$(print_unapproved)
-    message="The following $lines packages have licenses that are not in the approved list. Please investigate their licenses and add them to the approved list if compatible with MIT or remove the dependency\n$unapproved"
-    message="${message//'%'/'%25'}"
-    message="${message//$'\n'/'%0A'}"
-    message="${message//$'\r'/'%0D'}"
+    message="\033[1;31m$lines\033[0m packages have licenses that are not in the approved list. Please investigate their licenses and add them to the approved list if compatible with MIT or remove the dependency\n$unapproved"
     echo -e "::warning::$message"
     exit 0
 else
